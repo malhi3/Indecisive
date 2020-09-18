73dd27fae411ca09a1eab2e4c0b8f949784fb54b
malhi3 <malharthebestfriend@gmail.com>
2020-09-18 20:07:36 +0800

    First

---
+++ js/helpers.js
@@ -0,0 +1,28 @@
+function makeElement(type, attrs, txt) {
+  attrs = attrs || {};
+  txt = txt || '';
+
+  var el = document.createElement(type);
+
+  if (Object.keys(attrs).length !== 0) {
+    for (const [attr, val] of Object.entries(attrs)) {
+      el.setAttribute(attr, val);
+    }
+  }
+
+  if (txt != '') {
+    el.innerHTML = txt;
+  }
+
+  return el
+}
+
+function appendChildren(p, c) {
+  for (var child of c) {
+    if (Array.isArray(child)) {
+      appendChildren(p, child);
+    } else {
+      p.appendChild(child);
+    }
+  }
+}
