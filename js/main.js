18ba031aeb6f65c439780e017a5ae4be9c6409ad
malhi3 <malharthebestfriend@gmail.com>
2020-09-18 20:24:26 +0800

    Remove next btn at final screen

--- js/main.js
+++ js/main.js
@@ -319,6 +319,8 @@ function initActivities(){
 function displayResults() {
   window.scrollTo(0, 0);
 
+  removeElement('next-btn');
+
   var heading = makeElement('h1', {
     'class': 'section-heading'
   }, 'Your activities, sorted by how well they fit your criteria');
